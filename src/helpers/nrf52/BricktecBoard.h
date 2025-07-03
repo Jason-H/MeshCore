c269cee125840edfbe53d7becbe0f09c0208a74e
Jason Horrocks <brickles@devhack.net>
2025-07-03 16:26:32 -0700

    Update getBattMilliVolts to read VDD

--- src/helpers/nrf52/BricktecBoard.h
+++ src/helpers/nrf52/BricktecBoard.h
@@ -3,6 +3,7 @@
 #include "wiring_analog.h"
 #include <MeshCore.h>
 #include <Arduino.h>
+#include <cstdint>
 
 #define P_LORA_NSS 12 //P1.11
 #define P_LORA_DIO_1 15 //P0.02
@@ -25,8 +26,28 @@ public:
 
   void begin();
   uint16_t getBattMilliVolts() override {
-    //analogReadVDD();
-    return 3800;
+    // The promicro has unpopulated voltage dividers, so we must read the
+    // voltage directly from internal power supply pins using VDDHDIV5 and VDD
+
+    // Unfortunately, analogReadVDDHDIV5() is not available because PlatformIO
+    // has not yet updated the version of Adafruit_nRF52_Arduino to 1.7.0
+
+    // The VDD is downstream from an LDO voltage regulator should always be
+    // between 3.3V to about 1.8V, where the chip will brown out reset.
+
+    // adc range is 0 to 3.6V with 4096 - 1 as max
+    analogReadResolution(12);
+    uint32_t vddAnalogReading = analogReadVDD();
+    float divisor = 4095.0/3600.0;
+    uint16_t millivolts = vddAnalogReading / divisor;
+
+    // Treat anything above about 3.25V as 100%
+    if (vddAnalogReading >= 3250) {
+      return 4200;
+    }
+    else {
+      return vddAnalogReading / divisor ;
+    }
   }
   bool startOTAUpdate(const char* id, char reply[]) override;
 
